a001308f63b5d5d2491ef847704b2820367f5174
mubarak23 <mubarakaminu340@gmail.com>
2020-10-07 03:48:04 +0100

    setup nodemon and concurrency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
-//import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
@@ -17,6 +17,7 @@ const App = () => {
             <Route path="/product/:id" component={ProductScreen} />
           </Container>
         </main>
+        <Footer />
       </Router>
     </>
   );
