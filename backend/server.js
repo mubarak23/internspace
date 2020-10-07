8e604577f0c047ae1b726ae163ec8643977e800e
mubarak23 <mubarakaminu340@gmail.com>
2020-10-07 12:28:33 +0100

    connect route with frontend calls

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import color from "colors";
 import connectedDB from "./config/db.js";
 import products from "./data/products.js";
+import productRouter from "./routes/productRoutes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -14,14 +15,16 @@ app.get("/", (req, res) => {
   res.send("Api is runnung...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+//app.get("/api/products", (req, res) => {
+//res.json(products);
+//});
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+//app.get("/api/products/:id", (req, res) => {
+//const product = products.find((p) => p._id === req.params.id);
+//res.json(product);
+//});
+
+app.use("/api/products", productRouter);
 
 const PORT = process.env.PORT || 5000;
 
