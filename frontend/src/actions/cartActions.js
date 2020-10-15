2adf2e03a2dd616c3cbb307bff653c7d875ea8d0
mubarak23 <mubarakaminu340@gmail.com>
2020-10-15 20:02:05 +0100

    tough add cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { json } from "express";
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = await axios.get(`/api/product/${id}`);
+  const { data } = await axios.get(`/api/products/${id}`);
 
   dispatch({
     type: CART_ADD_ITEM,
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -29,7 +29,7 @@ const ProductScreen = ({ history, match }) => {
   //}, [match]);
   //const product = products.find((p) => p._id === match.params.id);
 
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetail);
   const { loading, error, product } = productDetails;
