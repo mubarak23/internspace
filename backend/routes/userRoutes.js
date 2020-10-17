ea2d6a58b3164501dbed7487228950ff630aea15
mubarak23 <mubarakaminu340@gmail.com>
2020-10-17 16:03:34 +0100

    auth user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import { authUser } from "../controllers/userController.js";
 const router = express.Router();
+router.options("/login", authUser);
 
 export default router;
