ff5c45efb6c188365b09c087ab1120dff3d04afa
mubarak23 <mubarakaminu340@gmail.com>
2020-10-29 15:14:19 +0100

    payment screen and it state

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Col, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormConnainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { savePaymentMethod } from "../actions/cartActions";
 
-const PaymentScreen = () => {
+const PaymentScreen = ({ history }) => {
+  const [paymentMethod, setPaymentMethod] = useState("paypal");
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+  if (!shippingAddress) {
+    history.push("/shipping");
+  }
+
+  const dispatch = useDispatch();
+  const onSubmthandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    history.push("/placeorder");
+  };
+
   return (
-    <div>
-      <h3>Payment Screenn</h3>
-    </div>
+    <FormConnainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={onSubmthandler}>
+        <Form.Group>
+          <Form.Label as="lagend">Select Payment Method</Form.Label>
+          <Col>
+            <Form.Check
+              type="radio"
+              label="PayPal or Credit Card"
+              name="paymentMethod"
+              value="paypal"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+            {/* <Form.Check
+              type='radio'
+              label='Stripe'
+              id='Stripe'
+              name='paymentMethod'
+              value='Stripe'
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check> */}
+          </Col>
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormConnainer>
   );
 };
 
