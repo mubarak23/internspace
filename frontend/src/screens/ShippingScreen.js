45b067f7911abea8a20dd2db51a5263efeccf080
mubarak23 <mubarakaminu340@gmail.com>
2020-10-29 08:32:43 +0100

    shipping address action and reducer

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const ShippingScreen = () => {
+  return (
+    <div>
+      <h2>Shipping Screen</h2>
+    </div>
+  );
+};
+
+export default ShippingScreen;
