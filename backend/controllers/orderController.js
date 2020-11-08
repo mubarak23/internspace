1f2f2fa355c8219d83b24e4ad49f059f4132dcac
mubarak23 <mubarakaminu340@gmail.com>
2020-11-08 11:15:29 +0100

    order pay action and reducers

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -49,9 +49,34 @@ const getOrderById = asyncHandler(async (req, res) => {
   if (order) {
     res.json(order);
   } else {
-    escape.status(404);
+    res.status(404);
     throw new Error("Order Not Found");
   }
 });
 
-export { addOrderItems, getOrderById };
+
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+
+const updateOrderToPaid = asyncHandler(async(req, res) => {
+    const order = await Order.findById(req.params.id)
+    if(order){
+      order.isPaid = true,
+      order.paidAt = Date.now()
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address = req.body.email_address
+      }
+     const updateOrder= await order.save();
+     res.json(updateOrder) 
+    }else{
+      res.status(404);
+    throw new Error("Order Not Found");
+    }
+})
+
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
