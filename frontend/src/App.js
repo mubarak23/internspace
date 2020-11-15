ff170478674935a47fe1c3e72015d4a2d662884a
mubarak23 <mubarakaminu340@gmail.com>
2020-11-15 08:06:30 +0100

    edit product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import OrderListScreen from "./screens/OrderListScreen";
 import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 const App = () => {
   return (
@@ -34,9 +35,13 @@ const App = () => {
             <Route path="/profile" component={ProfileScreen} />
             <Route path="/register" component={RegisterScreen} />
             <Route path="/admin/userlist" component={UserListScreen} />
+            <Route path="/admin/user/:id/edit" component={UserEditScreen} />
             <Route path="/admin/orderlist" component={OrderListScreen} />
             <Route path="/admin/productlist" component={ProductListScreen} />
-            <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+            <Route
+              path="/admin/product/:id/edit"
+              component={ProductEditScreen}
+            />
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/cart/:id?" component={CartScreen} />
           </Container>
---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+import { listProductDetails } from "../actions/productAction";
+//import { USER_UPDATE_RESET } from "../constants/userConstants";
+//import { updateUser } from "../actions/userActions";
+
+const ProductEditScreen = ({ match, history }) => {
+  const productId = match.params.id;
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
+  const [countInStock, setCountInStock] = useState("");
+  const [description, setDescription] = useState("");
+
+  const dispatch = useDispatch();
+  const productDetail = useSelector((state) => state.productDetail);
+  const { loading, error, product } = productDetail;
+
+  //const userUpdate = useSelector((state) => state.userUpdate);
+  //const {
+  //loading: loadingUpdate,
+  //error: errorUpdate,
+  //success: successUpdate,
+  //} = userUpdate;
+
+  useEffect(() => {
+    //if (successUpdate) {
+    //dispatch({ type: USER_UPDATE_RESET });
+    //history.push("/admin/userlist");
+    //}
+    if (!product.name || product._id !== productId) {
+      dispatch(listProductDetails(productId));
+    } else {
+      setName(product.name);
+      setPrice(product.price);
+      setCountInStock(product.countInStock);
+      setBrand(product.brand);
+      setDescription(product.description);
+      setImage(product.image);
+      setCategory(product.category);
+    }
+  }, [dispatch, history, productId, product]);
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+    console.log("submit eidt user");
+    //dispatch(updateUser({ _id: userId, name, email, isAdmin }));
+  };
+  return (
+    <>
+      <Link to="/admin/productlist">Go Back</Link>
+      <FormContainer>
+        <h2>Edit Product</h2>
+
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <form onSubmit={submitHandle}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                value={name}
+                placeholder="Enter Name"
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="price">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="image">
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter image url"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="brand">
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter brand"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="countInStock">
+              <Form.Label>Count In Stock</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter countInStock"
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="category">
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
+
+              <Form.Group controlId="description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+            </Form.Group>
+            <Button type="submit" variant="primary">
+              Update User
+            </Button>
+          </form>
+        )}
+      </FormContainer>
+    </>
+  );
+};
+
+export default ProductEditScreen;
