331dae4459198598e1e9e68958ec04e86ea4bc91
mubarak23 <mubarakaminu340@gmail.com>
2020-11-15 05:31:55 +0100

    create and update product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,4 +38,58 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Product",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample Brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    descriptio: "Sample Description",
+  });
+  const createProduct = await product.save();
+  res.json(createProduct);
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    user,
+    image,
+    brand,
+    category,
+    countInStock,
+    numReviews,
+    descriptio,
+  } = req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updateProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
 export { getProducts, getProductById, deleteProduct };
