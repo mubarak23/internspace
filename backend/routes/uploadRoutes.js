ce4796b492a58f89600f391bd8cc272c6c62a797
mubarak23 <mubarakaminu340@gmail.com>
2020-11-16 19:51:31 +0100

    front end inage upload

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -7,22 +7,23 @@ const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, "uploads/");
   },
-  filename(req, res, cb) {
+  filename(req, file, cb) {
     cb(
       null,
-      `${file.filename}-${Date.now()}${path.extname(file.originalname)}`
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
     );
   },
 });
 
 function checkFileType(file, cb) {
-  const fileTypes = /jpg|jpeg|png/;
-  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
-  const mimeType = fileTypes.test(file.mimeType);
-  if (extname && mimeType) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
     return cb(null, true);
   } else {
-    cb("Image only");
+    cb("Images only!");
   }
 }
 
