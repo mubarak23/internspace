f4d66c51105ae2cab6d452f1783ad3879e96016a
mubarak23 <mubarakaminu340@gmail.com>
2020-11-22 13:43:58 +0100

    top product carousel

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from "../models/productModel.js";
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 2;
+  const pageSize = 3;
   const page = Number(req.query.pageNumber) || 1;
   const keyword = req.query.keyword
     ? {
@@ -21,6 +21,14 @@ const getProducts = asyncHandler(async (req, res) => {
     .skip(pageSize * (page - 1));
 
   res.json({ products, page, pages: Math.ceil(count / pageSize) });
+});
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find().sort({ rating: -1 }).limit(3);
+  res.json(products);
 });
 
 // @desc    Fetch single product
@@ -149,4 +157,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
