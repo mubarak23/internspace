8f3440c1a98e4f031bd215210986095dcf24b983
mubarak23 <mubarakaminu340@gmail.com>
2020-11-23 18:38:54 +0100

    serving production

--- backend/server.js
+++ backend/server.js
@@ -52,17 +52,17 @@ app.use("/api/upload", uploadRouter);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
-
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(
