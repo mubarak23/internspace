bd254828f49eb37ba9fccdee9fbf73195af6f550
mubarak23 <mubarakaminu340@gmail.com>
2020-11-23 18:46:19 +0100

    resolve error

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,6 @@ if (process.env.NODE_ENV === "dev") {
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Api is runnung...");
-});
-
 //app.get("/api/products", (req, res) => {
 //res.json(products);
 //});
@@ -59,11 +55,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
 }
 const PORT = process.env.PORT || 5000;
 
