484f03e72f486478468c187df8315f6e5e71d21a
mubarak23 <mubarakaminu340@gmail.com>
2020-11-24 06:15:39 +0100

    resolve complete

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -24,6 +24,7 @@ import {
   USER_UPDATE_SUCCESS,
   USER_UPDATE_FAIL,
   USER_UPDATE_RESET,
+  USER_UPDATE_PROFILE_RESET,
 } from "../constants/userConstants.js";
 
 export const userRegisterReducer = (state = {}, action) => {
@@ -75,6 +76,7 @@ export const userUpdateProfileReducer = (state = { user: {} }, action) => {
       return { loading: true };
     case USER_UPDATE_PROFILE_SUCCESS:
       return { loading: false, success: true, userInfo: action.payload };
+
     case USER_UPDATE_PROFILE_FAIL:
       return { loading: false, error: action.payload };
     default:
