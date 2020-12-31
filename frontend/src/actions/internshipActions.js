7f8115a3bedc2960d0cfc69999cbb5a66551cec1
mubarak23 <mubarakaminu340@gmail.com>
2020-12-31 10:55:58 +0100

    create internship action

---
+++ frontend/src/actions/internshipActions.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import {
+  CREATE_INTERNSHIP_REQUEST,
+  CREATE_INTERNSHIP_SUCCESS,
+  CREATE_INTERNSHIP_FAIL,
+} from "../constants/internnshipConstant.js";
+
+export const crearteInternship = (internship) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: CREATE_INTERNSHIP_REQUEST,
+    });
+    const {
+      adminLogin: { adminInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${adminInfo.token}`,
+      },
+    };
+    const { data } = axios.post("/api/internship", internship, config);
+    dispatch({
+      type: CREATE_INTERNSHIP_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: CREATE_INTERNSHIP_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.response.message,
+    });
+  }
+};
